46893edc4c30694ac2a7f9ddd9aed73659ae87ec
deepin <deepin3809@gmail.com>
2023-07-23 22:02:27 +0900

    0723_side effect는 useEffect에 따로 뺌. -테스팅 할 때 유리. -부작용의 실행 타이밍을 제어 가능하다. -두번째 파라미터가 없으면 컴포넌트와 함께 실행된다. 값이 있으면 그 값이 변경 되었을 때 실행한다.
    함수가 정의 도리 때 함수 내에서 사용되는 변수는 함수 안에 가둬진다.
    이를 closer라고 함.
    
    set함수의 입력 값은 값이거나, 함수이다. 함수의 파라미터는 새로운 상태의 값이며, 리턴 값이 새로운 상태가 된다.
    
    useEffect의 리턴 값은 정리할 때 사용한다. unmount, 재실행될 때 자동으로 호출된다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import './App.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 function Counter({ title, initValue }) {
   const [count, setCount] = useState(initValue);
   const [step, setStep] = useState(1);
@@ -30,10 +30,32 @@ function Counter({ title, initValue }) {
     </div>
   );
 }
+
+function CounterUseEffect() {
+  const [count, setCount] = useState(0);
+  console.log('CounterUseEffect', count);
+  useEffect(() => {
+    setInterval(() => {
+      console.log('interval');
+      setCount(count + 1); //1초에 한번씩 카운트
+    }, 1000);
+  }, []);
+  // }, [count]); => 이와 같이 사용하면 카운트 값이 바뀔 때만  useEffect 가 실행됨
+  //  useEffect를 위와 같이 사용만 하면 컴포넌트가 랜더링 될 때 마다 돌아감
+  // 그래서 ,[] 배열을 넣어 한번만 실행하도록 만들어줌
+
+  return (
+    <div>
+      <h1>useEffect Counter</h1>
+      {count}
+    </div>
+  );
+}
 function App() {
   return (
     <div>
       <Counter title="불면증 카운터" initValue={10}></Counter>
+      <CounterUseEffect></CounterUseEffect>
     </div>
   );
 }
@@ -45,3 +67,6 @@ export default App;
 // 그래야 리액트는 이전의 상태와 이후의 상태가 변경되었다는 것을 알 수 있다.
 // 배열의 태그로 만들 때는 map함수를 사용한다. 콜백함수는 2개의 파라미터를 갖는다.
 // key값을 제공해야한다. key값은 목록 안 에서만 유일하면 된다.
+// set함수의 값은 값일 수도 있고 함수?일 수도 있다.
+// closer > 함수가 정의 될 때 그 함수가 사용하는 함수를 가둬두는 것
+// useEffect 가 return 값을 가질 수 있음 > but, 그 땐 함수를 사용해야만 가능
