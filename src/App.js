ab9627925d92c3ed853c23eae8069166416dea12
deepin <deepin3809@gmail.com>
2023-07-23 20:53:03 +0900

    0723_2타임_수정내역

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
-import "./App.css";
+// import './App.css';
 import { useState } from "react";
-
-function Counter(props) {
-  let countState = useState(props.initValue); //let 지역변수
-  let count = countState[0];
-  let setCount = countState[1];
-
-  let countState2 = useState(props.initValue);
-  let countDown = countState2[0];
-  let setCountDown = countState2[1];
-  console.log(countState);
+function Counter({ title, initValue }) {
+  const [count, setCount] = useState(initValue);
+  const [step, setStep] = useState(1);
+  const [history, setHistory] = useState([5, 5]);
   function up() {
-    console.log(countState);
-    setCount(count + 1);
+    const newCount = count + 1;
+    // setCount(count + 1);
+    history.push(newCount);
+    setHistory(history);
   }
-  function down() {
-    setCountDown(count - 1);
-  }
-  /* console.log(props);  자바스크립트 에서는 콘솔에 props(매개변수)값이 찍히게 함*/
+  const stepHandler = (evt) => {
+    setStep(Number(evt.target.value));
+  };
+  //위는 가운데 빈칸 숫자 변경하는 것에 대한 핸들러인듯?
   return (
     <div>
-      {/* <h1>카운터</h1>  - 텍스트가 아닌 함수형으로 불러오려면 */}
-      <h1>{props.title}</h1>
-      {/* <button onClick={up}>+</button> {props.initValue} 사용자 정의 내 컴포넌트 속성 불러오는 방법 */}
-      <button onClick={up}>+</button> {count}
-      <button onClick={down}>-</button> {countDown}
+      <h1>{title}</h1>
+      <button onClick={up}>+</button>
+      <input type="number" value={step} onChange={stepHandler} />
+      {count}
+      <ol>
+        {history.map((e, index) => (
+          <li key={index}>{e}</li>
+        ))}
+      </ol>
     </div>
   );
 }
-
 function App() {
   return (
     <div>
       <Counter title="불면증 카운터" initValue={10}></Counter>
-      {/* <Counter title="손님 카운터" initValue={10}></Counter> */}
-      {/*props 중괄호 안에 값을 넣으면 js의 데이터 타입으로 주입, 출력.
-      ""사용 시 문자열로 인식. 함수의 리턴값에서 중괄호 사용하면 변수로 간주함.*/}
     </div>
   );
 }
